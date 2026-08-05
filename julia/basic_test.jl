a3e3d258c39e6177e6cc5dba1c7093224307ffa8
Samuele Giuli <samuele.giuli@gmail.com>
2026-08-05 14:07:01 -0400

    Cleaned julia remainings

--- julia/basic_test.jl
+++
@@ -1,4 +0,0 @@
-using PyCall
-
-print_me(X::AbstractMatrix)=println(X)
-double_me(X::AbstractMatrix)=2 .* X 
--- julia/test/runtests.jl
+++
@@ -1,6 +0,0 @@
-using GGMPSSolver
-using Test
-
-@testset "GGMPSSolver.jl" begin
-    # Write your tests here.
-end
